cbf1e8170a6c97e5416b1e2076f4fa4d673fec7a
James Naugle <james.b.naugle@gmail.com>
2018-07-27 21:55:18 +0000

    More Structure

---
+++ app/Http/Controllers/VendorPaymentsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\vendor_payments;
+use Illuminate\Http\Request;
+
+class VendorPaymentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\vendor_payments  $vendor_payments
+     * @return \Illuminate\Http\Response
+     */
+    public function show(vendor_payments $vendor_payments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\vendor_payments  $vendor_payments
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(vendor_payments $vendor_payments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\vendor_payments  $vendor_payments
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, vendor_payments $vendor_payments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\vendor_payments  $vendor_payments
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(vendor_payments $vendor_payments)
+    {
+        //
+    }
+}

2ed0c4a15ec5ad0257af56789a118b0b7c7a4ba8
James Naugle <james.b.naugle@gmail.com>
2018-07-30 18:24:55 +0000

    debug

---
+++ database/migrations/2018_07_30_181230_create_sub_accounts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_accounts');
+    }
+}
